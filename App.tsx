a37f137a4fa5e089260a9efd1e90d484999ea4cc
Renato Rocha <renatob.rocha@gmail.com>
2020-07-26 00:22:12 -0300

    Rain

--- App.tsx
+++ App.tsx
@@ -1,12 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef } from 'react';
+import { StyleSheet, Dimensions, View, Image } from 'react-native';
+import Animated, { Clock, Easing, block, timing, cond, eq, set, not, Value, useCode, startClock, clockRunning, add } from "react-native-reanimated";
+const {height, width} = Dimensions.get("screen");
+const RAIN_DROP_SIZE = height * 0.02;
+
+const runProgress = (position: Value<number>, toValue: Animated.Adaptable<number>, clock: Clock) => {
+  
+  const state = { 
+    finished: new Value(0), 
+    position: new Value(0), 
+    frameTime: new Value(0), 
+    time: new Value(0) 
+  };
+
+  const config = { 
+    toValue, 
+    duration: 3000, 
+    easing: Easing.inOut(Easing.ease)
+  };
+
+  return block([
+    cond(not(clockRunning(clock)), [
+      startClock(clock),
+    ]),
+    timing(clock, state, config),
+    cond(eq(state.finished, 1), [
+      set(state.finished, 0),
+      set(state.frameTime, 0),
+      set(state.time, 0),
+      set(state.position, position)
+    ]),
+    state.position
+  ])
+  
+}
 
 export default function App() {
+  const position = useRef(new Value<number>(0)).current;
+  const toValue = useRef(new Value<number>(height)).current;
+
+  const elements = Array.from({length: 100}).map((_, index: number) => {
+    const horizontal = Math.random() * width;
+    const vertical = Math.random() * height;
+    const translateY = runProgress(position, add(toValue, vertical), new Clock());
+
+    return (
+      <Animated.View key={index.toString()} style={
+        [
+          styles.rain_wrapper,
+          {left: horizontal, top: -vertical}, 
+          {transform: [{translateY}]}
+        ]}>
+        <Animated.View style={styles.rain_drop}/>
+      </Animated.View>
+    )
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image style={StyleSheet.absoluteFillObject} source={require("./city.jpg")}/>
+      {elements}
+      <StatusBar style="light" hidden />
     </View>
   );
 }
@@ -14,8 +69,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ddd',
   },
+  rain_wrapper: {
+    position: "absolute",
+    top: -RAIN_DROP_SIZE/2,
+    left: width/2-RAIN_DROP_SIZE/2,
+  },
+  rain_drop: {
+    height: RAIN_DROP_SIZE,
+    width: RAIN_DROP_SIZE,
+    backgroundColor: "#fff",
+    
+    borderTopLeftRadius: RAIN_DROP_SIZE/2,
+    borderBottomLeftRadius: RAIN_DROP_SIZE/2,
+    borderTopRightRadius: RAIN_DROP_SIZE/2,
+
+    transform: [
+      {rotateZ: "-135deg"},
+    ] 
+  }
 });
